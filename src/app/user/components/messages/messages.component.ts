829b517e0faec0461c4aa5a5f2dafbfdf5581b06
beshoynadyy <beshoynadyy@gmail.com>
2023-03-28 11:41:35 +0200

    admin messages

--- src/app/user/components/messages/messages.component.ts
+++ src/app/user/components/messages/messages.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MessagesService } from '../../service/messages.service';
 
 @Component({
   selector: 'app-messages',
@@ -6,5 +7,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent {
+  messages: any[] = [];
+
+  selectedMessage: any | null = null;
+
+  constructor(private _messageService:MessagesService) { }
+
+  ngOnInit(): void {
+    this.getMessages()
+  }
+  getMessages(){
+    this._messageService.getAllMessages().subscribe({
+      next:(res)=>{
+                this.messages=res;
+                console.log(this.messages)
+                console.log("result done")
+              },
+              error:(err)=>{
+                console.log(err)
+                console.log("errorrrrrrrrrrrr")
+              }
+    })
+  }
+//  getProductFromWishlist(){
+//     this.productInWhislis=this._wishlisService.getAllWishlist().subscribe({
+//       next:(res)=>{
+//         this.productInWhislis=res;
+//         console.log(this.productInWhislis)
+//         console.log("result done")
+//       },
+//       error:(err)=>{
+//         console.log(err)
+//         console.log("errorrrrrrrrrrrr")
+//       }
+
+//     });
+//   }
+  showMessage(message: any): void {
+    this.selectedMessage = message;
+  }
+
+  copyEmail(email: string): void {
+    navigator.clipboard.writeText(email).then(() => {
+      console.log('Email copied to clipboard!');
+    }).catch(() => {
+      console.log('Failed to copy email. Please try again.');
+    });
+  }
 
 }
