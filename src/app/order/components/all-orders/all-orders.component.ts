d423cd5bac80defbd4d7d7410e07877c6537553f
mohameddawood21997 <mohameddawood21997@gmail.com>
2023-03-30 06:33:21 +0200

    order and order detail  last update

--- src/app/order/components/all-orders/all-orders.component.ts
+++ src/app/order/components/all-orders/all-orders.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-interface Item {
+import { AllOrdersService } from '../allOrders.service';
+interface Order {
   id:number;
-  customer: string;
-  email: string;
-  price: number;
+  name: string;
+  total_price: number;
   item: number;
-  status: string;
-  date: string;
+  payment_status: string;
+  created_at: string;
+  user:{
+    id:number,
+    email:string,
+  };
  
 }
 @Component({
@@ -15,100 +19,8 @@ interface Item {
   styleUrls: ['./all-orders.component.css']
 })
 export class AllOrdersComponent {
-  items: Item[] = [
-    {
-      id:1,
-      customer: 'ahmed',
-      email: 'ahmed@gmail.com',
-      price: 10.99,
-      item: 2, 
-      status: 'cancle',
-      date: '2023-03-18',
-    },
-    {
-      id:2,
-      customer: 'mohammmed',
-      email: 'mohammmed@gmail.com',
-      price: 100,
-      item: 3, 
-      status: 'delivered',
-      date: '2020-03-18',
-    },
-    {
-      id:3,
-      customer: 'ali',
-      email: 'ali@gmail.com',
-      price: 5,
-      item: 1, 
-      status: 'processing',
-      date: '2025-03-18',
-    },
-    {
-      id:4,
-      customer: 'sofo',
-      email: 'sofo@gmail.com',
-      price: 600,
-      item: 8, 
-      status: 'delivered',
-      date: '2023-03-18',
-    },
-    {
-      id:5,
-      customer: 'mar',
-      email: 'mar@gmail.com',
-      price: 50,
-      item: 9, 
-      status: 'cancle',
-      date: '2022-03-18',
-    },
-    {
-      id:6,
-      customer: 'aml',
-      email: 'aml@gmail.com',
-      price: 300,
-      item: 2, 
-      status: 'delivered',
-      date: '2023-03-18',
-    },
-    {
-      id:7,
-      customer: 'gege',
-      email: 'gege@gmail.com',
-      price: 16,
-      item: 2, 
-      status: 'processing',
-      date: '2021-03-18',
-    },
-    {
-      id:8,
-      customer: 'koko',
-      email: 'koko@gmail.com',
-      price: 108,
-      item: 6, 
-      status: 'cancle',
-      date: '2023-03-18',
-    },
-    {
-      id:9,
-      customer: 'lolo',
-      email: 'lolo@gmail.com',
-      price: 1102,
-      item: 5, 
-      status: 'processing',
-      date: '2023-03-18',
-    },
-    {
-      id:10,
-      customer: 'meme',
-      email: 'meme@gmail.com',
-      price: 10.99,
-      item: 2, 
-      status: 'delivered',
-      date: '2023-03-18',
-    },
-    
-  ];
-
+  orders: Order[] = [];
+  
   pageSize = 5;
   startIndex = 0;
   endIndex = this.pageSize;
@@ -116,10 +28,28 @@ export class AllOrdersComponent {
   pageNumbers: number[] = [];
   searchType = 'item';
   searchTerm = '';
-  filteredItems: Item[] = this.items;
+  filteredItems: Order[] = this.orders;
 
+  constructor(private orderServ:AllOrdersService){
+
+  }
+
+   getOrders(){
+    this.orderServ.getAllOrders().subscribe({
+      next:(res)=>{
+        this.orders=res;
+        this.filteredItems=this.orders;
+        console.log(this.orders);
+        
+  
+      }
+    })
+   }
   ngOnInit() {
+    console.log(10);
+    
     this.calculatePageNumbers();
+    this.getOrders();
   }
 
   calculatePageNumbers() {
@@ -147,34 +77,34 @@ export class AllOrdersComponent {
   }
 
   search() {
-    this.filteredItems = this.items.filter(item => {
+    this.filteredItems = this.orders.filter(item => {
       const searchTermLower = this.searchTerm.toLowerCase();
       
         if (this.searchType=='id'){
           return String(item.id).toLowerCase().includes(searchTermLower)
         }
         if(this.searchType=='price less than'){
-          return item.price<= +this.searchTerm;
+          return item.total_price<= +this.searchTerm;
         }
         if(this.searchType=='price bigger than'){
-          return item.price>= +this.searchTerm;
+          return item.total_price>= +this.searchTerm;
         }
         if (this.searchType=='customer'){
-          return item.customer.toLowerCase().includes(searchTermLower)
+          return item.name.toLowerCase().includes(searchTermLower)
         }
         if (this.searchType=='email'){
-          return item.email.toLowerCase().includes(searchTermLower)
+          return item.user.email.toLowerCase().includes(searchTermLower)
         }
         if (this.searchType=='status'){
-          return item.status.toLowerCase().includes(searchTermLower)
+          return item.payment_status.toLowerCase().includes(searchTermLower)
         }
         if (this.searchType=='item'){
           return String(item.item).toLowerCase().includes(searchTermLower)
         }
         
         return this.searchType === 'date'?
-        item.date.includes(searchTermLower):
-         item.date== this.searchTerm;
+        item.created_at.includes(searchTermLower):
+         item.created_at== this.searchTerm;
     });
     this.currentPage = 1;
     this.calculatePageNumbers();
